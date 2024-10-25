210c2ef89d9c07b2ab1d0ae4aa5c7c4ac2ff8e62
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 12:56:21 -0500

    rewrite the gewtbudget for current year use ES6

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,10 @@
+export default function getBudgetCurrentYear(income, gdp, capita) {
+    const budget = {
+        ['income-${getCurrentYear()}']: income,
+        ['gdp-${getCurrentYear()}']: gdp,
+        ['capita-${getCurrentYear()}']: capita,
+    };
+
+    return budget;
+}
+
