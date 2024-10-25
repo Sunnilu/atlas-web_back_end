c76e95a6923f186b9aecc0c78f5fcc160e6acfd0
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-24 19:19:00 -0500

    arrow function add

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,9 +1,12 @@
-export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = function add(newNeighborhood) {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
+const getNeighborhoodslist = () => {
+    const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+    const addNeighborhood = newNeighborhood => {
+        sanFranciscoNeighborhoods.push(newNeighborhood);
+        return { sanFranciscoNeighborhoods };
     };
-  }
+
+    return { sanFranciscoNeighborhoods, addNeighborhood };
+};
+   
+ export default getNeighborhoodslist;
