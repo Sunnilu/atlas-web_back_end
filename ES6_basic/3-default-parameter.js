93b2bdacf78bc6d1a257feb2929dee16ec9f1d78
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 11:50:27 -0500

    added regex to find assignment

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,5 @@
+// Regex pattern to find assignment: expansion1989\s*=\*89
+
 export default function getSumOfHoods(initialNumber, expansion1989 =89, expansion2019 = 19) {
     return initialNumber + expansion1989 + expansion2019;
 }
