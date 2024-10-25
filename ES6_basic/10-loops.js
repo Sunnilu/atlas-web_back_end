5cc0eb557b252b5ea16f6081d15113d09861595d
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 14:19:34 -0500

    npx eslint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = []; // Create a new array to hold modified values
-    for (let value of array) {
-      newArray.push(appendString + value); // Append the string and push to new array
-    }
-    return newArray; // Return the new array instead of modifying the original
+  const newArray = []; // Create a new array to hold modified values
+  for (const value of array) {
+    newArray.push(appendString + value); // Append the string and push to new array
   }
-  
-  
+  return newArray; // Return the new array instead of modifying the original
+}
