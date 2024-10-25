714c4fe9a697946cc559e2c24120b087c38f5b7f
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 13:08:18 -0500

    rewrite the get budget

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,10 +1,16 @@
-export default function getBudgetCurrentYear(income, gdp, capita) {
-    const budget = {
-        ['income-${getCurrentYear()}']: income,
-        ['gdp-${getCurrentYear()}']: gdp,
-        ['capita-${getCurrentYear()}']: capita,
-    };
-
-    return budget;
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
 }
 
+export default function getBudgetCurrentYear(income, gdp, capita) {
+    const currentYear = getCurrentYear();
+
+    const budget = {
+        ['income-${currentYear}']: income,
+        ['gdp-${currentYear}']: gdp,
+        ['capita-${currentYear}']: capita,
+    };
+
+    return budget
+}
