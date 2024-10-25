4d2f2eda1265861e54ee5ca187d0cfe488e59bcd
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 11:28:20 -0500

    2-arrow.js

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,38 +1,8 @@
-import React from 'react';
-
-// Define the component as a named function
-function NeighborhoodsList() {
-    const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-    
-    const addNeighborhood = newNeighborhood => {
-        sanFranciscoNeighborhoods.push(newNeighborhood);
-        return sanFranciscoNeighborhoods;
+export default function getNeighborhoodsList() {
+  
+    const self = this;
+    this.addNeighborhood = function add(newNeighborhood) {
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return self.sanFranciscoNeighborhoods;
     };
-
-    // Render JSX here
-    return (
-        <div>
-            <h1>San Francisco Neighborhoods</h1>
-            <ul>
-                {sanFranciscoNeighborhoods.map((neighborhood, index) => (
-                    <li key={index}>{neighborhood}</li>
-                ))}
-            </ul>
-            {/* Add a form to input new neighborhoods */}
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                const newNeighborhood = e.target.neighborhood.value.trim();
-                if (newNeighborhood) {
-                    addNeighborhood(newNeighborhood);
-                    e.target.reset();
-                }
-            }}>
-                <input type="text" name="neighborhood" placeholder="Add neighborhood" />
-                <button type="submit">Add</button>
-            </form>
-        </div>
-    );
-}
-
-// Export the component
-export default NeighborhoodsList;
+  }
