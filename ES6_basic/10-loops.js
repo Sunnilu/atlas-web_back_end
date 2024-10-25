36955ce320130f89f1e3d7295c34495bc9836980
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 14:16:24 -0500

    for loop not working

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let idx = 0; idx < array.length; idx++) {
-      array[idx] = appendString + array[idx];
+    for (let [index, value] of array.entries()) {
+      array[index] = appendString + value;
     }
   
     return array;
