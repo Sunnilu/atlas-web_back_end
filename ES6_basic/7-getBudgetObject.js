6fcb1a61e6a3e72f0d77b8e0f32ae5dee502c80a
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 12:42:08 -0500

    added regex

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,5 @@
+// Regex pattern to find budget object: const budget = \{\s*income, \s*gdp, \s*capita\s*\}
+
 export default function getBudgetObject(income, gdp, capita) {
     const budget ={
         income,
