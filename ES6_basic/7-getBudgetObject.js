00ea1d19774ca67d2e46a8a72ce78a12a3bd7660
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 12:46:26 -0500

    npx lint

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,11 +1,11 @@
 // Regex pattern to find budget object: const budget = \{\s*income, \s*gdp, \s*capita\s*\}
 
 export default function getBudgetObject(income, gdp, capita) {
-    const budget ={
-        income,
-        gdp,
-        capita,
-    };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
-    return budget
+  return budget;
 }
