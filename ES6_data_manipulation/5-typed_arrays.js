da8f50dfb73420e03880dbed5a21971608292ffd
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 12:43:49 -0500

    a function that returns a new ArrayBuffer with an Int8

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+// 5-typed_arrays.js
+// eslint-disable-next-line no-unused-vars
+export default function createInt8TypedArray(length, position, value) {
+  // check if the position is within the valid range
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  // Create a new ArrayBuffer and an Int8Array view
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+
+  return int8Array;
+}
