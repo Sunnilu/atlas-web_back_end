be45be2736c23aa734eba07e1adc0cc6036f0bab
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 11:29:47 -0500

    create a function that returns an array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// 0-get_list_students.js
+// eslint-disable-next-line no-unused-vars
+export default function getListStudents() {
+  return [
+    { id: 1, fistName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, fistName: ' James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
