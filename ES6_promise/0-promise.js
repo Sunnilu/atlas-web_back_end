90bc79d3ce41ae12039269c2b2dd72ad5deebb83
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 14:14:53 -0500

    had 2 module responses removed one

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,9 @@
 // 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Simulate an asynchronous operation (like an API call)
     setTimeout(() => {
-      // it can resolve with a value or reject with an error
-      resolve("Response from API");
-    }, 1000; // Simulate a delay of 1 second
+      resolve('Response from API');
+    }, 1000); // Simulate a delay of 1 second
   });
 }
