eaf23316ef9a5c2bae337458d2c15ee57e7b2b75
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 16:19:17 -0500

    corrected catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,13 +1,15 @@
-// Example function that returns a value
-const exampleFunction = () => 1000;
+// guardrail.js
+export default function guardrail(mathFunction) {
+  const queue = [];
 
-// Example function that throws an error
-const errorFunction = () => {
-  throw new Error('Something went wrong!');
-};
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
 
-// Using guardrail
-// eslint-disable-next-line no-undef
-console.log(guardrail(exampleFunction)); // Output: [1000, 'Guardrail was processed']
-// eslint-disable-next-line max-len, no-undef
-console.log(guardrail(errorFunction)); // Output: ['Something went wrong!', 'Guardrail was processed']
+  return queue;
+}
