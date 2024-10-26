aad17dcec552498664c1083232ebe2f185dae9d6
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 15:49:34 -0500

    eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-/* eslint-disable import/extensions */
 /* eslint-disable import/prefer-default-export */
+/* eslint-disable import/extensions */
 // 6-final-user.js
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
@@ -15,3 +15,5 @@ export async function handleProfileSignup(firstName, lastName, fileName) {
     value: result.status === 'fulfilled' ? result.value : result.reason,
   }));
 }
+// eslint-disable-next-line no-multiple-empty-lines
+
