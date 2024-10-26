ebc7ccdfd3e75313d603887d1fc5aa20de387525
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 12:26:32 -0500

    misspelled left the e off of grade

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,16 @@
 // 4-update_grade_by_city.js
-export default function updateStudentGradeByCity(students, city, newGrade) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   // Filter students by the specified city
   return students
     .filter((student) => student.location === city)
     .map((student) => {
       // Find the grade for the current student
-      const gradObj = newGrade.find(() => (grade) => grade.studentId === student.id);
+      const gradeObj = newGrades.find(() => (grade) => grade.studentId === student.id);
       return {
         id: student.id,
         firstName: student.firstName,
         location: student.location,
-        grade: gradObj ? gradObj.grade : 'N/A',
+        grade: gradeObj ? gradeObj.grade : 'N/A',
       };
     });
 }
