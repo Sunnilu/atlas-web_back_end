44e59774dd75d09f62adbecb7f2eba48db16d0e6
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 15:20:08 -0500

    write an export function

---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+export default function uploadPhoto(fileName) {
+  // eslint-disable-next-line no-template-curly-in-string
+  return Promise.reject(new Error('${fileName} cannot be processed'));
+}
