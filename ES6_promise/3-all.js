c67bb4e1ad77e0d4c6f2c9021aad65dd9175c6eb
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 16:35:47 -0500

    3 not working properly

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,21 +1,15 @@
-/* eslint-disable import/prefer-default-export */
-// 3-all.js
+/* eslint-disable import/extensions */
+import { uploadPhoto, createUser } from './utils.js';
 
-import { uploadPhoto, createUser } from './utils';
-
-export function handleProfileSignup() {
-  const firstName = 'Bob';
-  const lastName = 'Dylan';
-
-  Promise.all([uploadPhoto(), createUser(firstName, lastName)])
+// eslint-disable-next-line no-unused-vars
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
     // eslint-disable-next-line no-unused-vars
-    .then(([photoData, userData]) => {
-      console.log(`firstName: ${userData.firstName}`);
-      console.log(`lastName: ${userData.lastName}`);
+    .then(([photoResponse, userResponse]) => {
+      console.log(`First Name: ${userResponse.body.firstName}`);
+      console.log(`Last Name: ${userResponse.body.lastName}`);
     })
-    // eslint-disable-next-line no-unused-vars
-    .catch((error) => {
-      // eslint-disable-next-line quotes
-      console.error("Signup system offline");
+    .catch(() => {
+      console.error('Signup system offline');
     });
 }
