5ca0247bb3d64d0f2d30140af5d66cf8e7638261
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 14:34:30 -0500

    function handles promise

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+// 2-then.js
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error());
+}
