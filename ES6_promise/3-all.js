0285ae847c388d74862e2103484a1ac1c024ec70
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 14:59:33 -0500

    promise still not working

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,10 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((responses) => {
-      // eslint-disable-next-line no-undef, no-unused-vars
+      // eslint-disable-next-line no-unused-vars
       const [photoResponse, userResponse] = responses;
-      // eslint-disable-next-line no-template-curly-in-string
-      console.log('${userResponse.body.firstName} ${userResponse.body.lastName}');
+      console.log(`${userResponse.body.firstName} ${userResponse.body.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
