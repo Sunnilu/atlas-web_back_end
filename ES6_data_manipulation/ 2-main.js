a04ebfcf411b0702f8b8f48446e5ec8ff994c8d8
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 12:02:38 -0500

    2-main

--- ES6_data_manipulation/ 2-main.js
+++ ES6_data_manipulation/ 2-main.js
@@ -1,5 +1,7 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentsByLocation from "./2-get_students_by_loc.js";
+// eslint-disable-next-line import/extensions
+import getListStudents from './0-get_list_students.js';
+// eslint-disable-next-line import/extensions
+import getStudentsByLocation from './2-get_students_by_loc.js';
 
 const students = getListStudents();
 
