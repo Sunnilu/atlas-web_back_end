6bb76c3fd0a4a347e3a73a32f0e590fea2aff621
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 19:59:30 -0500

    changed not accepting the eslint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,11 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = this._validateSqft(sqft);
+
+    // Check if the subclass has overridden the method
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   // Getter for sqft
