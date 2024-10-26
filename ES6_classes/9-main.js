681ac62fd9ab8a605d0ced60435cfdf99860f901
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-25 21:02:33 -0500

    fixed 9 main

--- ES6_classes/9-main.js
+++ ES6_classes/9-main.js
@@ -1,9 +1,10 @@
-import listOfStudents from "./9-hoisting.js";
+// eslint-disable-next-line import/extensions
+import listOfStudents from './9-hoisting.js';
 
 console.log(listOfStudents);
 
 const listPrinted = listOfStudents.map(
-    student => student.fullStudentDescription
+  (student) => student.fullStudentDescription
 );
 
-console.log(listPrinted)
+console.log(listPrinted);
