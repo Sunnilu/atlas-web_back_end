65255f6163f39809c7fcdcf48d95114beaec0c06
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-26 15:27:55 -0500

    corrected output

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,9 @@
-/* eslint-disable no-unused-vars */
-export default function uploadPhoto(fileName) {
-  // eslint-disable-next-line no-template-curly-in-string
-  return Promise.reject(new Error('${fileName} cannot be processed'));
-}
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable import/no-self-import */
+import uploadPhoto from './5-photo-reject';
+
+describe('uploadPhoto', () => {
+  it('should reject with an error message', async () => {
+    await expect(uploadPhoto('guillaume.jpg')).rejects.toThrow('guillaume.jpg cannot be processed');
+  });
+});
