35c64e5d447c016f0fd28304271b7fbc12ff2bf0
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-09 23:54:16 +0000

    float n and returns its string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""type annotated function to_str that takes a float 'n' as argument and returns its string representation
+"""
+
+def to_str(n: float) -> str:
+    """ returns it's string representation
+
+    Args: 
+        n (float): the float to convert.
+    """
+    return str(n)
