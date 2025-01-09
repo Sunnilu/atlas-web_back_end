e96cbcc09e738ccd862d4d1791e3151d63a3f967
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-09 11:24:34 -0600

    added arguments

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,2 +1,16 @@
+#!/usr/bin/env python3
+"""
+Script for a type_annotated function 'add' that takes a float 'a' and a float 'b' as agruments and returns their sum as a float
+"""
+
 def add(a: float, b: float) -> float:
+    """Adds two floats and returns the sum.
+    
+    Args:
+        a (float): the first number.
+        b (float): the second number.
+
+    Returns:
+        float: The sum of 'a' and 'b'
+    """
     return a + b
