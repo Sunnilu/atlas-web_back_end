6ae6c5afea8caebc2369263fccbdfc84e13c0fba
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-10 18:57:00 +0000

    whitespace changes

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -19,9 +19,7 @@ async def wait_random(max_delay: int = 10) -> float:
     """
 
 
-    await asyncio.sleep(random.uniform(0, max_delay))
-
-    return random.uniform(0, max_delay)
+    delay = random.uiform(0,max_delay)
 
     await asyncio.sleep(delay)
 
