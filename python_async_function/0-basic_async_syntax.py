70e7027a459ed25c49ed7814684cec05c074262a
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-10 18:39:02 +0000

    reducing line 2 word count down to 79

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10) named wait_random that waits for a random delay between 0 and max_delay."""
+""" asynchronous coroutine that takes in an integer argument."""
 
 
 import asyncio
