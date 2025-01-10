712c2b3a2f1c17164d9971e3b68955f0915abb1b
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-10 00:39:35 +0000

    updated the sum

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,5 +3,12 @@
 
 from typing import List
 
-def sum_list(in_list: List[float]) -> float:
-    return sum(input_list)
+def sum_list(input_list: List[float]) -> float:
+    """which takes a list input_list of
+    floats as argument and returns their sum as a float.
+    """
+    sum = 0
+    for i in input_list:
+        if type(i) == float:
+            sum += i
+    return sum
