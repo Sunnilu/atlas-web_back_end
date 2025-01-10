d452b76b85e52d69bad530c9f12adb4782c88980
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-10 20:57:42 +0000

    task wait random imported

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,8 +4,8 @@
 import asyncio
 from typing import List
 
-#Take the code from wait_n and alter it into a new function task_wait_n.
-wait_n = __import__('1-concurrent_coroutines').wait_n
+# Import task_wait_random from the correct file
+task_wait_random = __import__('1-concurrent_coroutines').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
@@ -25,3 +25,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     results = await asyncio.gather(*tasks)
     
     return results
+
