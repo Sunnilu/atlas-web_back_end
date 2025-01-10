f71244826541c958fbfb66ea7de1aa603a1e6660
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-10 02:18:35 +0000

    added second

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,17 +5,19 @@
 from typing import List, Tuple
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """
-    Takes a list of strings and returns a list of tuples, where each tuple contains 
-    a string and its length.
-
-    Parameters:
-    lst (List[str]): A list of strings.
-
-    Returns:
-    List[Tuple[str, int]]: A list of tuples where each tuple contains a string 
-    and its length as an integer.
-    """
+    jls_extract_var = """
+        Takes a list of strings and returns a list of tuples, where each tuple contains 
+        a string and its length.
+    
+        Parameters:
+        lst (List[str]): A list of strings.
+        2nd (List[Tulpes]): A string and its length.
+    
+        Returns:
+        List[Tuple[str, int]]: A list of tuples where each tuple contains a string 
+        and its length as an integer.
+        """
+    jls_extract_var
     
     # Create a list of tuples containing the string and its length
     return [(i, len(i)) for i in lst]
