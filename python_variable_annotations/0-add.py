84be3b9cac4afb1aa026b8b61bf50c51f76624d9
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-11 00:53:59 +0000

    pep8

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -6,7 +6,7 @@ Script for a type_annotated function returns their sum as a float
 
 def add(a: float, b: float) -> float:
     """Adds two floats and returns the sum.
-    
+
     Args:
         a (float): the first number.
         b (float): the second number.
@@ -16,4 +16,3 @@ def add(a: float, b: float) -> float:
     """
 
     return a + b
-    
