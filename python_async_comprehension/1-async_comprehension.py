dd176fc13655ae8258773ddaad7fd7667e8793c4
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-11 03:07:07 +0000

    documented and float validated

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,16 +4,16 @@
 
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
-    Asynchronous generator that yields a random integer between 0 and 10,
+    Asynchronous generator that yields a random float between 0 and 10,
     one value per second, for 10 iterations.
 
     Yields:
-        int: A random integer between 0 and 10.
+        float: A random number between 0 and 10.
 
     Returns:
         None: This generator does not return any value when exhausted.
@@ -23,21 +23,27 @@ async def async_generator() -> Generator[int, None, None]:
         yield random.randint(0, 10)  # Yield a random integer between 0 and 10
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> list[float]:
     """
-    Asynchronously collects 10 random integers from the async_generator
+    Asynchronously collects 10 random numbers from the async_generator 
     using an async comprehension and returns them as a list.
 
+    This coroutine demonstrates the usage of async comprehension to 
+    gather values from the async_generator and accumulate them in a list.
+
     Returns:
-        List[int]: A list of 10 random integers collected from async_generator.
+        list: A list of 10 random integers collected from async_generator.
     """
     return [num async for num in async_generator()]
 
 
 async def main():
     """
-    Runs the async_comprehension coroutine and prints the collected random
-    integers.
+    Runs the async_comprehension coroutine and prints the collected 
+    random integers.
+
+    This function demonstrates how to call the async_comprehension 
+    function and print the resulting list of random numbers.
     """
     random_numbers = await async_comprehension()
     print(random_numbers)
@@ -46,6 +52,3 @@ async def main():
 # Run the main function to see the output
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
