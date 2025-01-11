16f21e6773c8d0fb9a36d80d495973c79bef28a9
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-11 01:03:26 +0000

    corrected for i added if isinstance

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -10,6 +10,6 @@ def sum_list(input_list: List[float]) -> float:
     """
     sum = 0
     for i in input_list:
-        if type(i) == float:
+       if isinstance(i, float): 
             sum += i
     return sum
