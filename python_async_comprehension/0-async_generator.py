8d1d4fec50f483cf11ed11ca2f8229c22534b633
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-11 02:37:11 +0000

    async generator tht takes no arguments

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Async-Generator that takes no arguments"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)  # Wait for 1 second asynchronously
+        yield random.randint(0, 10)  # Yield a random number between 0 and 10
+
+
+async def main():
+    async for value in async_generator():
+        print(value)
+
+
+# Run the main function to see the output
+if __name__ == "__main__":
+    asyncio.run(main())
