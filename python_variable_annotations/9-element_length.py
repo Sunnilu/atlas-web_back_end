9ac744845d15d648be81bde284cc3dfc91fa7817
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-11 01:25:49 +0000

    updated

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
-"""Annotate the below functions parameters and return values with the appropriate types"""
-
+"""A function that returns a list of tuples containing and its length."""
 
 from typing import List, Tuple
 
+
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    jls_extract_var = """
-        Takes a list of strings and returns a list of tuples, where each tuple contains 
-        a string and its length.
-    
-        Parameters:
+    """Returns a list of tuples where each tuple contains a string and its length.
+
+    Args:
         lst (List[str]): A list of strings.
-        2nd (List[Tulpes]): A string and its length.
-    
-        Returns:
-        List[Tuple[str, int]]: A list of tuples where each tuple contains a string 
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples, where each tuple contains a string 
         and its length as an integer.
-        """
-    jls_extract_var
-    
-    # Create a list of tuples containing the string and its length
+    """
+    # List comprehension that creates a list of tuples with each string and its length
     return [(i, len(i)) for i in lst]
+
