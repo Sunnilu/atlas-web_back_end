98ac1fb68ed5795ff4129b0d2c70afca7a6c1fb2
Suntha Lucas <Suntha.Lucas@atlasschool.com>
2025-01-11 00:46:24 +0000

    pep8

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -2,10 +2,12 @@
 """type annotated function to_str that takes a float 'n' as argument and returns its string representation
 """
 
+
 def to_str(n: float) -> str:
     """ returns it's string representation
 
     Args: 
         n (float): the float to convert.
     """
+    
     return str(n)
