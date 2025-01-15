0ee089d8a92ebe379ae26aa4f71b5eccac5550b4
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-15 15:26:39 -0600

    testing

--- caching/2-main.py
+++ caching/2-main.py
@@ -16,3 +16,33 @@ my_cache.put("F", "Mission")
 my_cache.print_cache()
 my_cache.put("G", "San Francisco")
 my_cache.print_cache()
+
+
+def test_lifo_cache():
+    # Test 1: MAX_ITEMS = 1, add 5 items
+    print("Test 1: MAX_ITEMS = 1, adding 5 items")
+    cache = LIFOCache()
+    BaseCaching.MAX_ITEMS = 1
+    for i in range(1, 6):
+        cache.put(str(i), f"Item {i}")
+        cache.print_cache()
+    print()
+
+    # Test 2: MAX_ITEMS = 2, add 10 items
+    print("Test 2: MAX_ITEMS = 2, adding 10 items")
+    cache = LIFOCache()
+    BaseCaching.MAX_ITEMS = 2
+    for i in range(1, 11):
+        cache.put(str(i), f"Item {i}")
+        cache.print_cache()
+    print()
+
+    # Test 3: MAX_ITEMS = 5, add 10 items
+    print("Test 3: MAX_ITEMS = 5, adding 10 items")
+    cache = LIFOCache()
+    BaseCaching.MAX_ITEMS = 5
+    for i in range(1, 11):
+        cache.put(str(i), f"Item {i}")
+        cache.print_cache()
+
+test_lifo_cache()
