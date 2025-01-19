310095b23b15488e8e7ffb10e4cf0422b482db92
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-19 16:14:55 -0600

    annotated

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python3
-'''Encrypting passwords getting valid password'''
-
+'''Encrypting passwords and validating the password'''
 
 import bcrypt
 
-
-def has_password(password:str) -> bytes:
-    """ Description: Implement hash_password function one str argument
-        returns a salted, hashed password, which is a byte string.
-        Use the bcrypt package to perform the hashing (with hashpw).
+def has_password(password: str) -> bytes:
+    """ 
+    Description: Implement hash_password function with one str argument
+    Returns a salted, hashed password, which is a byte string.
+    Uses bcrypt package to perform the hashing (with hashpw).
     """
-     pass_encoded = password.encode()
+    pass_encoded = password.encode()
     pass_hashed = bcrypt.hashpw(pass_encoded, bcrypt.gensalt())
-
     return pass_hashed  
 
-
-is_valid(hashed_password: bytes, password: str) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     '''
-       Description: Implement an is_valid function that expects 2 arguments and
-                    returns a boolean.
-       Arguments:   hashed_password: bytes type
-                    password: string type
-       Use bcrypt to validate that the provided password matches the hashed
-       password.
+    Description: Implement an is_valid function that expects 2 arguments and
+                 returns a boolean.
+    Arguments:   
+        hashed_password: bytes type
+        password: string type
+    Use bcrypt to validate that the provided password matches the hashed password.
     '''
-    valid = False
     pass_encoded = password.encode()
-    if bcrypt.checkpw(pass_encoded, hashed_password):
-        valid = True
-    return valid
+    return bcrypt.checkpw(pass_encoded, hashed_password)
