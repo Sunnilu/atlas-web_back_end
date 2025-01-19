dd85a884a266becb796ccab755c5b6702a4afcd5
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-19 16:09:01 -0600

    annotated

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,6 +8,7 @@ import bcrypt
 def has_password(password:str) -> bytes:
     """ Description: Implement hash_password function one str argument
         returns a salted, hashed password, which is a byte string.
+        Use the bcrypt package to perform the hashing (with hashpw).
     """
      pass_encoded = password.encode()
     pass_hashed = bcrypt.hashpw(pass_encoded, bcrypt.gensalt())
