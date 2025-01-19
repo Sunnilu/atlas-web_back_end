22ba5c892156f39f5d70b72219763a543edea141
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-18 19:49:48 -0600

    annotated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     return re.sub(r'(' + '|'.join(map(re.escape, fields)) + r')' + re.escape(separator) + r'[^' + re.escape(separator) + r']*', lambda m: m.group(0).replace(m.group(0).split(separator)[-1], redaction), message)
