6e69195d4ff78851bf1571eed70aaed6ffabbe34
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-22 08:32:28 -0600

    basic auth module

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic authentication module
+"""
+from api.v1.auth.auth import Auth  # Assuming Auth class is defined in api.v1.auth.auth
+
+class BasicAuth(Auth):
+    """ BasicAuth class inherits from Auth.
+    For now, it remains empty but can be extended in the future.
+    """
+    pass
