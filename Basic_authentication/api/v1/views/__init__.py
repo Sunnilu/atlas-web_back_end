8fa34021c245161f5204464920fb3e0f964e9c2b
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-23 10:57:21 -0600

    rearranged the stucture

--- Basic_authentication/api/v1/views/__init__.py
+++ Basic_authentication/api/v1/views/__init__.py
@@ -5,5 +5,6 @@ from flask import Blueprint
 # Define the Blueprint object for app_views
 app_views = Blueprint('app_views', __name__)
 
-# Import views (routes) that will be registered to this blueprint
+# Avoid circular imports by only importing views *after* the Blueprint is defined
+# Import specific view modules after app_views is set up
 from api.v1.views import some_view_module  # Import specific view modules
