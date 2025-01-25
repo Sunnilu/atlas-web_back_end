26516a53098c01e8da35a81d6ae5a4427ac6424c
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-24 21:25:39 -0600

    still not working

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,60 +1,65 @@
 #!/usr/bin/env python3
-""" 
-4-mru_cache module
-This module defines the MRUCache class, which inherits from BaseCaching.
-The MRUCache class implements a caching system that uses the Most Recently Used (MRU) eviction policy.
-When the cache exceeds the maximum allowed size, it discards the most recently used item.
+"""
+4-mru_cache module.
+This module defines the MRUCache class, which is a subclass of BaseCaching.
+It implements the Most Recently Used (MRU) eviction policy for caching.
 """
 
 class BaseCaching:
     """
     BaseCaching is a base class for caching systems.
-    It initializes the cache_data dictionary and sets the maximum allowed number of items.
+
+    Attributes:
+        MAX_ITEMS (int): Maximum number of items allowed in the cache.
+        cache_data (dict): Dictionary to store cached items.
     """
-    MAX_ITEMS = 4  # Default maximum number of items allowed in the cache
+    MAX_ITEMS = 4  # Default maximum number of items in the cache
 
     def __init__(self):
         """
         Initializes the cache_data as an empty dictionary.
         """
-        self.cache_data = {}
+        self.cache_data = {}  # Dictionary to store the cached items
+
 
 class MRUCache(BaseCaching):
     """
-    MRUCache is a class that implements the Most Recently Used (MRU) eviction policy.
-    If the cache exceeds the maximum number of items, the most recently used item is discarded.
+    MRUCache class that implements the Most Recently Used (MRU) eviction policy.
+
+    In this policy, when the cache exceeds the maximum allowed number of items (MAX_ITEMS),
+    the most recently used item is discarded.
     """
-    
+
     def __init__(self):
         """
-        Initializes the MRUCache and calls the parent class constructor to set up the cache_data.
+        Initializes the MRUCache. Calls the parent class constructor to initialize cache_data.
         """
         super().__init__()
 
     def put(self, key, item):
         """
-        Adds a new item to the cache with the given key.
-        If the cache exceeds MAX_ITEMS, it evicts the most recently used item.
+        Adds an item to the cache with the given key.
 
-        If either the key or item is None, the method does nothing.
+        If the number of items exceeds MAX_ITEMS, it evicts the most recently used item.
 
         Args:
-            key (str): The key for the item.
+            key (str): The key to associate with the item.
             item (str): The item to store in the cache.
+
+        If key or item is None, the method does nothing.
         """
-        # If key or item is None, do nothing
         if key is None or item is None:
             return
         
-        # Insert the item into the cache
+        # Insert or update the item in the cache
         self.cache_data[key] = item
         
-        # If the cache exceeds the maximum number of items, evict the most recently used item
+        # If the cache exceeds MAX_ITEMS, evict the most recently used item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # Get the most recently added key (the last key in the dictionary)
+            # Get the most recently added key (the last one in the dictionary)
             mru_key = list(self.cache_data.keys())[-1]
             
-            # Discard the most recently added item
+            # Remove and discard the most recently added item
             self.cache_data.pop(mru_key)
             
             # Print the discarded key
@@ -63,25 +68,25 @@ class MRUCache(BaseCaching):
     def get(self, key):
         """
         Retrieves the value associated with the given key from the cache.
-        
-        If the key is None or doesn't exist in the cache, returns None.
 
         Args:
-            key (str): The key for which the value is to be retrieved.
+            key (str): The key to retrieve the value for.
 
         Returns:
             str or None: The value associated with the key, or None if the key does not exist.
         """
-        if key is None:
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key, None)
+        
+        return self.cache_data.get(key)
 
     def print_cache(self):
         """
-        Prints the current state of the cache in the following format:
+        Prints the current state of the cache in the format:
         Current cache:
         A: Hello
         B: World
+        C: Holberton
         """
         print("Current cache:")
         for key, value in self.cache_data.items():
