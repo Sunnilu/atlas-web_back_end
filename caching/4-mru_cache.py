ce8a50215b43c963ddb2292790bb5dd6cdfc2434
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-24 21:19:42 -0600

    working

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
-'''MRU Cach that inherits from BashCaching'''
 
 class BaseCaching:
     """
     BaseCaching is a base class for caching systems.
-    It initializes the cache data and sets a maximum number of items allowed in the cache.
+    It initializes the cache_data dictionary and sets the maximum allowed cache size.
     """
     MAX_ITEMS = 4  # Maximum number of items allowed in the cache
 
     def __init__(self):
         """
-        Initializes the cache data as an empty dictionary.
-        This will be used to store the cached items.
+        Initializes the cache_data dictionary to store the cached items.
         """
-        self.cache_data = {}  # Dictionary to store cache data
+        self.cache_data = {}  # Dictionary to store cache items
 
 
 class MRUCache(BaseCaching):
     """
-    MRUCache is a caching system that inherits from BaseCaching and implements the Most Recently Used (MRU)
-    eviction policy. If the cache exceeds the MAX_ITEMS limit, it discards the most recently used item.
+    MRUCache is a class that implements the Most Recently Used (MRU) eviction policy.
+    When the cache exceeds the maximum size, it discards the most recently used item.
+    This class inherits from the BaseCaching class.
     """
 
     def __init__(self):
         """
-        Initializes the MRUCache.
-        Calls the parent class (BaseCaching) constructor to set up the cache_data dictionary.
+        Initializes the MRUCache and calls the parent class constructor to initialize cache_data.
         """
         super().__init__()
 
@@ -33,25 +31,22 @@ class MRUCache(BaseCaching):
         """
         Adds an item to the cache with the given key.
 
-        If the cache exceeds the MAX_ITEMS limit, it evicts the most recently used (MRU) item.
+        If the cache exceeds MAX_ITEMS, it evicts the most recently used item.
 
         Args:
             key (str): The key to associate with the item.
             item (str): The item to store in the cache.
 
-        If key or item is None, this method does nothing.
-
-        If the number of items in the cache exceeds MAX_ITEMS, the most recently added item will be discarded.
-        The key of the discarded item will be printed in the format: "DISCARD: <key>"
+        If key or item is None, the method does nothing.
         """
         # If key or item is None, do nothing
         if key is None or item is None:
             return
-        
+
         # Insert the item into the cache
         self.cache_data[key] = item
         
-        # Check if the number of items exceeds the maximum allowed
+        # If the cache exceeds the maximum number of items, evict the most recently used item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             # Get the most recently added key (the last key in the dictionary)
             mru_key = list(self.cache_data.keys())[-1]
