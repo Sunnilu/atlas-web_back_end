b3c22d9c14693fdedf63e5a95ce61c1f1fc5f91c
Suntha <Suntha.Lucas@atlasschool.com>
2025-01-27 03:35:26 +0000

    app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,10 +2,10 @@
 """
 Route module for the API
 """
-
-
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -14,6 +14,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -25,20 +31,36 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
+    """
+    Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def unauthorized(error) -> str:
+    """
+    Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """
+    handler before_request
+    """
+    authorized_list = ['/api/v1/status/',
+                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    if auth and auth.require_auth(request.path, authorized_list):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
-# Testing the new error handler
-from api.v1.views import index  # Assuming index.py is in the same directory as api/v1/views/__init__.py
-
-@index.route("/api/v1/unauthorized")
-def unauthorized_endpoint():
-    abort(401)  # Raise a 401 error to trigger the custom error handler
-    return "This line will not be executed"  # This line is unreachable because abort raises an exception
