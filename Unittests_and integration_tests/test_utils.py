fff5280ff0aabf76eb2da9e5790792037fb0ec47
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-02 12:09:44 -0600

    reworking of test untils.py

--- Unittests_and integration_tests/test_utils.py
+++ Unittests_and integration_tests/test_utils.py
@@ -1,85 +1,32 @@
 #!/usr/bin/env python3
-"""unitest module
-"""
+""" A Function that test the utils.py file."""
 import unittest
-from typing import Dict, Tuple, Union
-from unittest.mock import patch, Mock
 from parameterized import parameterized
-
-from utils import (
-    access_nested_map,
-    get_json,
-    memoize,
-)
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
+    """
+    Test class for the access_nested_map function from the utils module.
+    Inherits from unittest.TestCase.
+    """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-    ) -> None:
-        """Tests `access_nested_map`'s output."""
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Test the access_nested_map function with different inputs.
+        Parameters:
+        nested_map (Mapping): The nested map to access.
+        path (Sequence): The sequence of keys representing the path to
+        the value.
+        expected (Any): The expected result from the function.
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            exception: Exception,
-    ) -> None:
-        """Tests `access_nested_map`'s exception raising."""
-        with self.assertRaises(exception):
-            access_nested_map(nested_map, path)
 
-
-class TestGetJson(unittest.TestCase):
-    """Tests the `get_json` function."""
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    def test_get_json(
-            self,
-            test_url: str,
-            test_payload: Dict,
-    ) -> None:
-        """Tests `get_json`'s output."""
-        attrs = {'json.return_value': test_payload}
-        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
-            self.assertEqual(get_json(test_url), test_payload)
-            req_get.assert_called_once_with(test_url)
-
-
-class TestMemoize(unittest.TestCase):
-    """Tests the `memoize` function."""
-
-    def test_memoize(self) -> None:
-        """Tests `memoize`'s output."""
-        class TestClass:
-            def a_method(self):
-                return 42
-
-            @memoize
-            def a_property(self):
-                return self.a_method()
-        with patch.object(
-                TestClass,
-                "a_method",
-                return_value=lambda: 42,
-        ) as memo_fxn:
-            test_class = TestClass()
-            self.assertEqual(test_class.a_property(), 42)
-            self.assertEqual(test_class.a_property(), 42)
-            memo_fxn.assert_called_once()
+if __name__ == "__main__":
+    unittest.main()
