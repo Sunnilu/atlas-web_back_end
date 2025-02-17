80dd641644be0c5dc2dc515be5619f00ae76155b
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-17 10:58:56 -0600

    commit all

---
+++ MySQL_Advanced/ 0-uniq_users.sql
@@ -0,0 +1,5 @@
+    CREATE TABLE users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  -- Auto-increment and primary key
+        email VARCHAR(255) NOT NULL UNIQUE,      -- Email, not null, and unique
+        name VARCHAR(255)                            -- Name
+    );
