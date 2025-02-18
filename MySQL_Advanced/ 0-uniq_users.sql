a65c7657ed087b66c25ed989eaa073e73846d394
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2025-02-18 14:52:31 -0600

    Update  0-uniq_users.sql
    left out the if not exist user

--- MySQL_Advanced/ 0-uniq_users.sql
+++ MySQL_Advanced/ 0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- a SQL script that creates a table user
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
