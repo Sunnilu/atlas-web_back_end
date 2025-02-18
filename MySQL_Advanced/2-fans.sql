45fa21c8c37e12968fec26cbd6dc876f25fe2b33
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 16:18:56 -0600

    rewrite

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -3,24 +3,17 @@
 -- Column names must be: origins and nb fan_fans --
 
 -- Create index for partition optimization
-CREATE INDEX idx_origin ON bands(origin);
-WITH country_fans AS (
-    SELECT 
-        origin,
-        COUNT(*) as nb_fans
-    FROM bands
-    GROUP BY origin
-),
-ranked_countries AS (
+
+DELIMITER //
+
+CREATE PROCEDURE RankCountriesByFans()
+BEGIN
     SELECT 
         origin,
         nb_fans,
         DENSE_RANK() OVER (ORDER BY nb_fans DESC) as rank
-    FROM country_fans
-)
-SELECT 
-    rank,
-    origin,
-    nb_fans
-FROM ranked_countries
-ORDER BY nb_fans DESC;
+    FROM metal_bands
+    ORDER BY nb_fans DESC;
+END //
+
+DELIMITER ;
