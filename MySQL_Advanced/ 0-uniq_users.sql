16a96e3076c8f0e5304ae806f75050e77c3ed6d1
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 14:42:59 -0600

    create table users

--- MySQL_Advanced/ 0-uniq_users.sql
+++ MySQL_Advanced/ 0-uniq_users.sql
@@ -1,8 +1,5 @@
--- Create a SQL script that creates a table --
----
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  
-    email VARCHAR(255) NOT NULL UNIQUE,    
-    name VARCHAR(255)  
-    PRIMARY KEY (id)                          -
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
