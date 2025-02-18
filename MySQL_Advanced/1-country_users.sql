eb6c1f74f47438ea1c8ddbe76cf9f16d64b9b2a4
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 14:57:50 -0600

    create a user table with country enum

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
 -- Create a table users with country enum --
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    PRIMARY KEY (id),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+    name VARCHAR(255),
+    country VARCHAR(2) NOT NULL DEFAULT 'US'
 );
+
+ALTER TABLE users ADD CONSTRAINT chk_country CHECK (country IN ('US', 'CO', 'TN'));
