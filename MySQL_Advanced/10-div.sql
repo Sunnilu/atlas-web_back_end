b3a839062863acda1d4cf7d3a4bf96abbf035aa3
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 16:40:59 -0600

    SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10,4)
+DETERMINISTIC
+BEGIN
+    DECLARE result DECIMAL(10,4);
+    
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    
+    SET result = a / b;
+    RETURN result;
+END //
+
+DELIMITER ;
