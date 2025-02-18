da38e89db068c9b8318eaf280e3d1c0e12d958d6
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 16:21:49 -0600

    rewrite

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -6,9 +6,31 @@ CREATE TRIGGER decrease_item_quantity
 BEFORE INSERT ON orders
 FOR EACH ROW
 BEGIN
+    DECLARE existing_quantity INT;
+    
+    -- Get current quantity
+    SELECT quantity INTO existing_quantity 
+    FROM items WHERE name = NEW.item_name;
+    
+    -- Update quantity
     UPDATE items 
-    SET quantity = quantity - NEW.quantity_ordered
-    WHERE item_id = NEW.item_id;
+    SET quantity = existing_quantity - NEW.number
+    WHERE name = NEW.item_name;
+    
+    -- Log the change
+    INSERT INTO inventory_changes (
+        item_name,
+        old_quantity,
+        new_quantity,
+        change_type,
+        changed_at
+    ) VALUES (
+        NEW.item_name,
+        existing_quantity,
+        existing_quantity - NEW.number,
+        'ORDER_DEDUCTION',
+        NOW()
+    );
 END//
 
 DELIMITER ;
