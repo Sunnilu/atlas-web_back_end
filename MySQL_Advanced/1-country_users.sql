d59ca364ac7187336dbfddc6f1a712b1be898182
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 17:52:21 -0600

    removed the second constraint

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS users (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country VARCHAR(2) NOT NULL DEFAULT 'US'
+    -- country VARCHAR(2) NOT NULL DEFAULT 'US'
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
 
-ALTER TABLE users ADD CONSTRAINT chk_country CHECK (country IN ('US', 'CO', 'TN'));
+-- ALTER TABLE users ADD CONSTRAINT chk_country CHECK (country IN ('US', 'CO', 'TN'));
