e75566345e8278ea6dff4c32098cefe03b7b0f57
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 20:20:54 -0600

    list all documents

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
-'''Python function that lists all documents in a collection'''
+'''lists all documents in a collection'''
 
 
 def list_all(mongo_collection):
-    """
-    Lists all documents in a MongoDB collection.
+    """ Returns an empty list if no document in the collection """
     
-    Args:
-        mongo_collection: A PyMongo collection object
+    cursor = mongo_collection.find()
     
-    Returns:
-        list: A list of all documents in the collection. Returns empty list if no documents exist.
-    """
-    try:
-        # Use find() to get a cursor for all documents
-        cursor = mongo_collection.find()
-        
-        # Convert the cursor to a list
-        # This will return an empty list if no documents exist
-        return list(cursor)
-    except Exception as e:
-        # Log the error and return an empty list
-        print(f"An error occurred: {str(e)}")
-        return []
+    return[doc for doc in cursor]
+    
