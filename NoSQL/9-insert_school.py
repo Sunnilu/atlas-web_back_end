617afb4ffb7e173c1a6ebac37a1a6133083268a8
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-19 09:14:14 -0600

    install pymongo

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 """
-a function that inserts a new document in a collection based on kwargs
+This script contains a function to insert a new school document into a MongoDB collection.
+
+Function:
+    - insert_school: Inserts a new document into the specified MongoDB collection
+      using keyword arguments and returns the generated _id.
+
+Requirements:
+    - pymongo library: Ensure you have pymongo installed.
 """
 
 from pymongo import MongoClient
 
 def insert_school(mongo_collection, **kwargs):
-    # Insert the document into the collection with the provided kwargs
+    """
+    Inserts a new school document into the given MongoDB collection.
+
+    Parameters:
+        mongo_collection (pymongo.collection.Collection): The MongoDB collection object
+        **kwargs: The fields and values to be inserted into the document.
+
+    Returns:
+        str: The _id of the newly inserted document.
+    """
+    # Insert the document using the keyword arguments provided
     result = mongo_collection.insert_one(kwargs)
-    # Return the _id of the new document
+    # Return the _id of the newly inserted document
     return result.inserted_id
