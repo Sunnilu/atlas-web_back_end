4e0d2801ab024cff0c6b6dc66a36c5382863ec45
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-18 19:00:05 -0600

    remove deterministic

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,9 +1,8 @@
 -- SQL SafeDiv the first by the second number or returns 0 number is equal to 0.
 
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
+CREATE FUNCTION SafeDivi(a INT, b INT)
 RETURNS FLOAT
-DETERMINISTIC
 BEGIN
     DECLARE result FLOAT;
     
