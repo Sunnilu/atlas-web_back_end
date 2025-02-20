21a5ce31c4203a6b0eaf59c1aee0bb1831bd16e5
Suntha <Suntha.Lucas@atlasschool.com>
2025-02-20 12:03:28 -0600

    added docstring

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 '''Provides stats about Nginx logs stored in MongoDB'''
 
-
 from pymongo import MongoClient
 
-
 def log_stats():
+    '''
+    Logs statistics from Nginx logs stored in MongoDB.
+    
+    This function connects to a MongoDB database, queries the logs collection,
+    and prints the following statistics:
+    1. The total number of logs.
+    2. The count of each HTTP method (GET, POST, PUT, PATCH, DELETE).
+    3. The count of GET requests with path '/status'.
+    
+    No parameters or return values.
+    '''
     # Connect to MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs  # The database 'logs'
