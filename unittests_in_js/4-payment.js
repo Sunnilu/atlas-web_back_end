af31e65d881fd03a607a9c350fe87262d52c6562
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-02 17:46:09 -0600

    functio to always return the value 10

---
+++ unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+//4-payment.js
+const utils = require('./utils');
+
+function calculateTotal(a, b) {
+    const result = Utils.calculateNumber('SUM', a, b);
+    console.log(' The total is: ' + result);
+}
+
+module.exports = calculateTotal;
