89be43289217cc09d7c13ce01e5083d91c96d09c
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-02 14:57:11 -0600

    define a function calculateNumber inside it

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,10 @@
+// utils.js
+const utils = {
+    calculateNumber: (type a, b) => {
+        if (type === 'SUM') {
+            return a + b;
+        }
+        // If type is not 'SUM', then it is 'SUBTRACT'
+}
+
+module.exports = utils; 
