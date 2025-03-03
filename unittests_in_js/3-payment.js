3b57ab5172ccf232a6102d387445612ecd82d028
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-03 16:32:09 -0600

    utils sendpaymentrequest to API corrected

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,10 +1,9 @@
-//3-payment.js
+// 3-payment.js
 const utils = require('./utils');
-
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
     const result = utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${total}`); 
+    console.log(`The total is: ${result}`);  // Changed 'total' to 'result'
 }
 
 module.exports = sendPaymentRequestToApi;
