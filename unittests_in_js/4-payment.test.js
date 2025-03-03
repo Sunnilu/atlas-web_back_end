f3754a132b5dc963cefd11c5064708ba48be55db
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-03 17:54:03 -0600

    rewrite

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,32 +1,37 @@
 // 4-payment.test.js
 const sinon = require('sinon');
-const calculateTotal = require('./4-payment');
 const Utils = require('./utils');
-const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToAPI with stubs', () => {
-  let calculateNumberStub;
-  let consoleSpy;
+describe('sendPaymentRequestToApi', () => {
+    let calculateNumberStub;
+    
+    beforeEach(() => {
+        // Create a stub that returns 10 for any input
+        calculateNumberStub = sinon.stub(Utils, 'calculateNumber')
+            .returns(10);
+        
+        // Create a spy for console.log
+        sinon.spy(console, 'log');
+    });
 
-  beforeEach(() => {
-    // Stub the Utils.calculateNumber function to always return 10
-    calculateNumberStub = sinon.spy(console, 'log'); // spy on console.log
-  });
+    afterEach(() => {
+        // Clean up after each test
+        calculateNumberStub.restore();
+        console.log.restore();
+    });
 
-  afterEach(() => {
-    // Restore the stub and spy after each test
-    calculateNumberStub.restore();
-    console.restore();
-  });
+    it('should call calculateNumber with correct parameters', () => {
+        sendPaymentRequestToApi(100, 20);
+        
+        // Verify the stub was called with correct parameters
+        sinon.assert.calledWith(calculateNumberStub, 'SUM', 100, 20);
+    });
 
-  it('validates that Utils.calculateNumber was called with the right arguments and sonsole.log with the correct message', () => {
-    sendPaymentRequestToApi(100, 20);
-
-    // Verify that the stub was called with the correct parameters
-    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true
-
-    // Verify that console.log was called with the expected message
-    expect(consoleSpy.calledOnceWithExactly(' The total is: 10')).to.be.true;
-  });
+    it('should log correct message', () => {
+        sendPaymentRequestToApi(100, 20);
+        
+        // Verify console.log was called with the expected message
+        sinon.assert.calledWith(console.log, 'The total is: 10');
+    });
 });
