d16fcfdbeb1fb42679fe9918dac5e8186d903f03
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-03 15:07:03 -0600

    removed test case from chai.js

--- unittests_in_js/2-calcul_chai.js
+++ unittests_in_js/2-calcul_chai.js
@@ -10,14 +10,6 @@ function add(a, b) {
   // Test suite using Chai's expect for BDD-style tests
   const { expect } = require('chai');
   
-  // Test cases
-  describe('Math functions', function() {
-    describe('#add()', function() {
-      it('should return the sum of two numbers', function() {
-        const result = add(1, 2);
-        expect(result).to.equal(3); // Chai's expect style
-      });
-    });
   
     describe('#subtract()', function() {
       it('should return the difference between two numbers', function() {
