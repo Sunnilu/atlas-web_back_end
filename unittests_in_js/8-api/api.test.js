c5a8b74a10fec5df0f1e230371b3800cd088179c
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-04 09:32:21 -0600

    adjusted IT

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,50 +1,47 @@
 // api.test.js
-const request = require('request');
-const chai = require('chai');
-const expect = chai.expect
+const request = require("request");
+const chai = require("chai");
+const expect = chai.expect;
 
-describe('cart page', () => {
-    it('should return correct status code when id is a number', (done) => {
-        request(app)
-            .get('/cart/123')
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
+describe("cart page", () => {
+  it("should return status 200", function (done) {
+    request(url, function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
+  });
 
-    it('should return 404 when id is not a number', (done) => {
-        request(app)
-            .get('/cart/abc')
-            .expect(404)
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
-    });
+  it("should return 404 when id is not a number", (done) => {
+    request(app)
+      .get("/cart/abc")
+      .expect(404)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
 
-    it('should return correct payment methods when id is valid', (done) => {
-        request(app)
-            .get('/cart/123')
-            .expect(200)
-            .expect({
-                methods: ['Credit Card', 'PayPal', 'Bank Transfer']
-            })
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
-    });
+  it("should return correct payment methods when id is valid", (done) => {
+    request(app)
+      .get("/cart/123")
+      .expect(200)
+      .expect({
+        methods: ["Credit Card", "PayPal", "Bank Transfer"],
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
 
-    it('should return error message when id is invalid', (done) => {
-        request(app)
-            .get('/cart/abc')
-            .expect(404)
-            .expect('Invalid cart ID')
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
-    });
+  it("should return error message when id is invalid", (done) => {
+    request(app)
+      .get("/cart/abc")
+      .expect(404)
+      .expect("Invalid cart ID")
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
 });
