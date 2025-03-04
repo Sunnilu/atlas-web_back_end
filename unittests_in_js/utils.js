968cb7a41d2dee257e74718aac9272c68bfa1de5
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-03 20:06:37 -0600

    went back to old utils

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -1,9 +1,19 @@
-// utils.js
-const Utils = {
-  calculateNumber: function(type, a, b) {
+// utils.js (example)
+function calculateNumber(type, a, b) {
+  a = Math.round(a);  // Round a
+  b = Math.round(b);  // Round b
+
+  if (type === 'SUM') {
       return a + b;
+  } else if (type === 'SUBTRACT') {
+      return a - b;
+  } else if (type === 'DIVIDE') {
+      if (b === 0) {
+          throw new Error('Cannot divide by 0');
+      }
+      // Round the result to 1 decimal place
+      return Math.round((a / b) * 10) / 10;
   }
-};
+}
 
-module.exports = Utils;
-
+module.exports = { calculateNumber };
