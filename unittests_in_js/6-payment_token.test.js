0a190e8406ba6251c486061c53d010be45bd90d0
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-03 18:04:02 -0600

    revised getpayment token should expected true

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,35 +1,16 @@
 // 6-payment_token.test.js
-
+const sinon = require('sinon');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-
-  it('should return successful response when success is true', (done) => {
-    // Call the function with 'true' to simulate a successful response
-    getPaymentTokenFromAPI(true).then((response) => {
-      // Test if the response contains the expected data
-      expect(response).toEqual({ data: 'Successful response from the API' });
-     
-      // Call done() to let Jest know the async test is complete
-      done();
-    }).catch((error) => {
-      // If there is an error, fail the test
-      done(error);
+    it('should return the expected data when success is true', (done) => {
+        getPaymentTokenFromAPI(true)
+            .then((result) => {
+                sinon.assert.match(result, { data: 'Successful response from the API' });
+                done();
+            })
+            .catch((error) => {
+                done(error);
+            });
     });
-  });
-
-  it('should not return any response when success is false', (done) => {
-    // Call the function with 'false' to simulate no response
-    getPaymentTokenFromAPI(false).then((response) => {
-      // Expect that no response is returned
-      expect(response).toBeUndefined();
-
-      // Call done() to indicate the test is complete
-      done();
-    }).catch((error) => {
-      // If there is an error, fail the test
-      done(error);
-    });
-  });
-
 });
