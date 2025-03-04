b6e6a312d8fbe53391eacdbd4d2cf5f43deb9901
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-04 15:02:53 -0600

    fix urls

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+// const router = express.Router();
 
-app.use(express.json());
+// app.use(express.json());
 // app.use('/', router);
 
 app.get('/', (req, res) => {
@@ -18,12 +18,12 @@ const getPaymentMethods = () => ({
 });
 
 // GET /available_payments endpoint
-router.get('/available_payments', (req, res) => {
+app.get('/available_payments', (req, res) => {
     res.json(getPaymentMethods());
 });
 
 // POST /login endpoint
-router.post('/login', (req, res) => {
+app.post('/login', (req, res) => {
     const { userName } = req.body;
     if (!userName) {
         return res.status(400).json({ error: 'Username is required' });
@@ -32,7 +32,7 @@ router.post('/login', (req, res) => {
 });
 
 // GET /cart/:id endpoint
-router.get('/cart/:id', (req, res) => {
+app.get('/cart/:id', (req, res) => {
     const id = req.params.id;
     
     // Validate that id is a number
