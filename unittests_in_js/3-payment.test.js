c21e65a735ceaa7a7531a1a6b9223a685fe5d294
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-03 20:08:50 -0600

    went back to old 3-test

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,9 +1,42 @@
-// 3-payment.js
-const Utils = require('./utils');
+// 3-payment.test.js
+const sinon = require('sinon');
+const { expect } = require('chai');
+const utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${total}`);
-}
+// Test Suite for sendPaymentRequestToApi
+describe('sendPaymentRequestToApi', () => {
+    let spy;
 
-module.exports = sendPaymentRequestToApi;
+    beforeEach(() => {
+        // Create a spy for utils.calculateNumber
+        spy = sinon.spy(utils, 'calculateNumber');
+    });
+
+    afterEach(() => {
+        // Restore the original function after each test
+        spy.restore();
+    });
+
+    it('should call utils.calculateNumber with SUM, 100, 20', () => {
+        // Call the function with test values
+        sendPaymentRequestToApi(100, 20);
+
+        // Verify that calculateNumber was called with correct arguments
+        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    });
+
+    it('should log the correct total', () => {
+        // Spying on console.log to check what is being logged
+        const logSpy = sinon.spy(console, 'log');
+
+        // Call the function with test values
+        sendPaymentRequestToApi(100, 20);
+
+        // Verify that console.log was called with the correct message
+        expect(logSpy.calledWith('The total is: 120')).to.be.true;
+
+        // Restore console.log after test
+        logSpy.restore();
+    });
+});
