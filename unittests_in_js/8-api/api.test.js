56a5b4da2d200b6ed87574c539c848d6d2fc7aed
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-04 09:43:32 -0600

    "/"

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,47 +1,24 @@
-// api.test.js
-const request = require("request");
-const chai = require("chai");
+const request = require('request');
+const chai = require('chai');
 const expect = chai.expect;
-
-describe("cart page", () => {
-  it("should return status 200", function (done) {
-    request(url, function (error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
+describe('Index page', function() {
+    const url = 'http://localhost:7865/';
+    it('should return status 200', function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
-  });
-
-  it("should return 404 when id is not a number", (done) => {
-    request(app)
-      .get("/cart/abc")
-      .expect(404)
-      .end((err, res) => {
-        if (err) return done(err);
-        done();
-      });
-  });
-
-  it("should return correct payment methods when id is valid", (done) => {
-    request(app)
-      .get("/cart/123")
-      .expect(200)
-      .expect({
-        methods: ["Credit Card", "PayPal", "Bank Transfer"],
-      })
-      .end((err, res) => {
-        if (err) return done(err);
-        done();
-      });
-  });
-
-  it("should return error message when id is invalid", (done) => {
-    request(app)
-      .get("/cart/abc")
-      .expect(404)
-      .expect("Invalid cart ID")
-      .end((err, res) => {
-        if (err) return done(err);
-        done();
-      });
-  });
+    it('should return Welcome to the payment system', function(done) {
+        request(url, function(error, response, body) {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+    it('should return correct content-type header', function(done) {
+        request(url, function(error, response, body) {
+            expect(response.headers['content-type']).to.include('text/html');
+            done();
+        });
+    });
 });
