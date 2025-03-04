3678ee3d9fed858778c27ae68a20c66211cc2152
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-04 10:59:20 -0600

    changed IT

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,24 +1,30 @@
-const request = require('request');
-const chai = require('chai');
+const request = require("request");
+const chai = require("chai");
 const expect = chai.expect;
-describe('Index page', function() {
-    const url = 'http://localhost:7865/cart/';
-    it('should return status 200', function(done) {
-        request(url + '1', function(error, response, body) {
-            expect(response.statusCode).to.equal(200);
-            done();
-        });
+describe("Index page", function () {
+  const url = "http://localhost:7865/";
+  it("should return status 200", function (done) {
+    request(url + "cart/1", function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
-    it('should return 404 with wrong id type', function(done) {
-        request(url + 'a', function(error, response, body) {
-            expect(response.statusCode).to.equal(404);
-            done();
-        });
+  });
+  it("should return 404 with wrong id type", function (done) {
+    request(url + "cart/a", function (error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
     });
-    it('should return correct content-type header', function(done) {
-        request(url, function(error, response, body) {
-            expect(response.headers['content-type']).to.include('text/html');
-            done();
-        });
+  });
+  it("should return status 200", function (done) {
+    request(url, function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
+  });
+  it("should return the correct content", function (done) {
+    request(url, function (error, response, body) {
+      expect(body).to.equal("Welcome to the payment system");
+      done();
+    });
+  });
 });
