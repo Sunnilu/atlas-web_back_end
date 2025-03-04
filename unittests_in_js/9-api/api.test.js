69b5864bbc2127d83f71f466ead017d243237d86
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-04 10:18:26 -0600

    added /cart and concat the url

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,49 +1,24 @@
-// api.test.js
-const request = require('supertest');
-const app = require('./api');
-
-describe('cart page', () => {
-    it('should return correct status code when id is a number', (done) => {
-        request(app)
-            .get('/cart/123')
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+describe('Index page', function() {
+    const url = 'http://localhost:7865/cart/';
+    it('should return status 200', function(done) {
+        request(url + '1', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
     });
-
-    it('should return 404 when id is not a number', (done) => {
-        request(app)
-            .get('/cart/abc')
-            .expect(404)
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
+    it('should return 404 with wrong id type', function(done) {
+        request(url + 'a', function(error, response, body) {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
     });
-
-    it('should return correct payment methods when id is valid', (done) => {
-        request(app)
-            .get('/cart/123')
-            .expect(200)
-            .expect({
-                methods: ['Credit Card', 'PayPal', 'Bank Transfer']
-            })
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
-    });
-
-    it('should return error message when id is invalid', (done) => {
-        request(app)
-            .get('/cart/abc')
-            .expect(404)
-            .expect('Invalid cart ID')
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
+    it('should return correct content-type header', function(done) {
+        request(url, function(error, response, body) {
+            expect(response.headers['content-type']).to.include('text/html');
+            done();
+        });
     });
 });
