12365ae0be8095118d82c6e7a44ec96d7a406394
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-03 19:52:20 -0600

    3-payment updated

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,22 +1,9 @@
-const sinon = require('sinon');
+// 3-payment.js
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
-const { expect } = require('chai');
 
-describe('sendPaymentRequestToApi', () => {
-  let spy;
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${total}`);
+}
 
-  beforeEach(() => {
-    spy = sinon.spy(Utils, 'calculateNumber');
-  });
-
-  afterEach(() => {
-    spy.restore();
-  });
-
-  it('validates that Utils.calculateNumber was called with the correct arguments', () => {
-    sendPaymentRequestToApi(100, 20);
-
-    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-  });
-});
+module.exports = sendPaymentRequestToApi;
