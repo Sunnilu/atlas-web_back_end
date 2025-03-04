8868d0d7339d431df44767856ce1298109082319
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-04 14:53:48 -0600

    update

--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -3,6 +3,8 @@ const app = require("./api");
 const chai = require("chai");
 const expect = chai.expect;
 
+const url = "http://localhost:7865/";
+
 describe("API Endpoints", () => {
   // Available Payments Tests
   describe("GET /", () => {
@@ -12,11 +14,31 @@ describe("API Endpoints", () => {
         done();
       });
     });
+    
     it("should return Welcome to the payment system", function (done) {
       request('http://localhost:7865/', function (error, response, body) {
         expect(body).to.equal("Welcome to the payment system");
         done();
       });
+    });
+  });
+
+  it("should return status 200", function (done) {
+    request(url + "cart/1", function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it("should return 404 with wrong id type", function (done) {
+    request(url + "cart/a", function (error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+  it("should return the correct content", function (done) {
+    request(url + 'cart/124', function (error, response, body) {
+      expect(body).to.equal("Payment methods for cart 124");
+      done();
     });
   });
 
