b7da474cd104fa5564aadb519bb5f4d1e5b0dd56
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-05 07:55:47 -0600

    It accepts two arguments schoolName, and value. It should set in Redis the value for the key schoolName It should display a confirmation message using redis.print displaySchoolValue: It accepts one argument schoolName. It should log to the console the value for the key passed as argument

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', function() {
+    console.log('Connected to Redis!');
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, function(err, reply) {
+        if (err) {
+            console.log(`Error getting ${schoolName}: ${err}`);
+            return;
+        }
+        console.log(reply);
+    });
+}
+
+// Call the functions as specified
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
