1c6b41079de2a74ea91b512713d9bc91aaa0ed7d
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-06 08:00:04 -0600

    8.test.js

---
+++ queuing_system_in_js/8-job.test.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const kue = require('kue');
+const createPushNotificationsJobs = require('./8-job');
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  before(() => {
+    // Enter test mode before running tests
+    queue = kue.createQueue();
+    queue.testMode.enter();
+  });
+
+  after(() => {
+    // Exit test mode and clear the queue after tests
+    queue.testMode.exit();
+    queue.testMode.clear();
+  });
+
+  it('should throw an error when jobs is not an array', () => {
+    assert.throws(() => createPushNotificationsJobs('not an array', queue), 
+      Error, 'Jobs is not an array');
+  });
+
+  it('should create jobs in the queue', () => {
+    const jobs = [
+      { phoneNumber: '4153518780', message: 'Test message 1' },
+      { phoneNumber: '4153518781', message: 'Test message 2' }
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    // Validate jobs in queue
+    assert.strictEqual(queue.testMode.jobs.length, 2);
+    assert.strictEqual(queue.testMode.jobs[0].type, 'push_notification_code_3');
+    assert.strictEqual(queue.testMode.jobs[1].type, 'push_notification_code_3');
+  });
+
+  it('should handle empty array', () => {
+    const jobs = [];
+    createPushNotificationsJobs(jobs, queue);
+    assert.strictEqual(queue.testMode.jobs.length, 0);
+  });
+
+  it('should create jobs with correct data', () => {
+    const jobs = [
+      { phoneNumber: '4153518780', message: 'Test message 1' }
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+    const job = queue.testMode.jobs[0];
+    assert.deepStrictEqual(job.data, jobs[0]);
+  });
+});
