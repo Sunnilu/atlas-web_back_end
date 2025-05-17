c991c807bb5e065653fdd9d825046a6cdbeeffad
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-17 13:52:41 -0500

    rewrite

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -43,9 +43,8 @@ class Server:
             }
 
         return self.__indexed_dataset
+        def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict[str, Any]:
         """Return a page of the dataset with deletion-resilience.
 
         Args:
