0d05a92b607dc5dcd8e39b253514f3a8dfc5905f
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-17 16:27:13 -0500

    implemented is_valid function that expects 2 agruments and returns a boolean

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,11 +5,10 @@ Encrypt password module
 
 import bcrypt
 
-
 def hash_password(password: str) -> bytes:
     """
     Hashes a password using bcrypt with a randomly generated salt.
-
+    
     Args:
         password (str): The password to hash.
 
@@ -17,3 +16,17 @@ def hash_password(password: str) -> bytes:
         bytes: The salted, hashed password.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if the provided password matches the hashed password.
+    
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The plain-text password to verify.
+
+    Returns:
+        bool: True if the password is correct, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
