e17b18d2510be9ad1a3d2fa0675293b28549934c
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-18 14:52:07 -0500

    added extra paths uers/

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -36,7 +36,8 @@ def before_request():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/users/',
     ]
 
     if not auth.require_auth(request.path, excluded_paths):
