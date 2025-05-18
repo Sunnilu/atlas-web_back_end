0349eada323c66af9bb47542957319572f5f9ed5
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-17 19:41:31 -0500

    added a method def extract user credintials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,22 +3,24 @@
 BasicAuth module for handling Basic Authentication.
 """
 
-from api.v1.auth.auth import Auth
 import base64
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth. """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
-        Extracts the Base64 part Authorization header for Basic Authentication.
+        Extracts the Base64 part of the Authorization header for Basic Auth.
 
         Args:
             authorization_header (str): The full Authorization header.
 
         Returns:
-            str or None: The Base64 encoded part of header if valid, else None.
+            str or None: The Base64 encoded part of the header if valid,
+                         else None.
         """
         if authorization_header is None:
             return None
@@ -31,9 +33,10 @@ class BasicAuth(Auth):
 
         return authorization_header.split(' ', 1)[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """
-        Decodes the Base64 string base64_authorization_header to UTF-8 string.
+        Decodes the Base64 string base64_authorization_header to UTF-8.
 
         Args:
             base64_authorization_header (str): The Base64 encoded string.
@@ -52,3 +55,26 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts the user email and password from the decoded Base64 string.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded string.
+
+        Returns:
+            Tuple[str, str] or (None, None): email and password or (None, None)
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
