4b3b3c118ce79800873c8c3f2e9fc536a0cbc733
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-17 19:35:36 -0500

    more corrections

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,7 +12,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
-        Extracts the Base64 part of Authorization header for Basic Authentication.
+        Extracts the Base64 part Authorization header for Basic Authentication.
 
         Args:
             authorization_header (str): The full Authorization header.
