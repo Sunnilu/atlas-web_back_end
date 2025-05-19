2d441212c127fda3c0c094f593bdeaf679a7f9f6
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-19 15:15:43 -0500

    corrected errors

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 """
 Initializer for API view blueprints.
-Registers all route modules for the API.
 """
 
 from flask import Blueprint
 
-# Create the blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Import views to register routes
-from api.v1.views.index import *
+# Import all views
 from api.v1.views.users import *
-
-# Import session login view function only (not app_views to avoid circular import)
+from api.v1.views.index import *
 from api.v1.views.session_auth import session_login
 
-# Register the session login route explicitly
+# Register session route safely here to avoid circular import
 app_views.add_url_rule(
     '/auth_session/login',
     view_func=session_login,
@@ -24,7 +20,4 @@ app_views.add_url_rule(
     strict_slashes=False
 )
 
-
-
-)
 
