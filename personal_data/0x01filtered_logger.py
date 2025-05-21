854fdf0e668c571704d54ac18803ab3d60b4652e
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-21 16:45:15 -0500

    changed to re.sub()

--- personal_data/0x01filtered_logger.py
+++ personal_data/0x01filtered_logger.py
@@ -3,12 +3,10 @@
 Module that logs filtered user data from a MySQL database.
 """
 import re
-from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
-    Obfuscate specified fields in a log message using a redaction string.
-    """
-    return re.sub(rf'({"|".join(fields)})=.*?{separator}', 
-                  lambda m: f"{m.group(1)}={redaction}{separator}", 
-                  message)
+    return re.sub(
+        rf'({"|".join(fields)})=[^{separator}]*',
+        lambda match: f"{match.group(1)}={redaction}",
+        message
+    )
