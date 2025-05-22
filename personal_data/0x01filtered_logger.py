d15ab1c5bb646c8e9ec6879c2fabc40922ec4d98
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-22 13:10:47 -0500

    still running trying to get the re.sub to work

--- personal_data/0x01filtered_logger.py
+++ personal_data/0x01filtered_logger.py
@@ -2,16 +2,10 @@
 """
 Module that logs filtered user data from a MySQL database.
 """
+
 import re
-import os
-import logging
-import mysql.connector
 from typing import List
 
-
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    return re.sub(
-        rf'({"|".join(fields)})=[^{separator}]*',
-        lambda match: f"{match.group(1)}={redaction}",
-        message
-    )
+    return re.sub(rf'({"|".join(fields)})=[^{separator}]*',
+                  lambda m: f"{m.group(1)}={redaction}", message)
